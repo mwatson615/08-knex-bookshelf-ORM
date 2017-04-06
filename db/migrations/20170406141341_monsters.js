fa2d47f8508b215d5de1198f28e9b15c40b58ea8
Michael Watson <m.watson.tn@gmail.com>
2017-04-06 14:57:39 -0500

    migration and rollback success for monsters table

--- db/migrations/20170406141341_monsters.js
+++ db/migrations/20170406141341_monsters.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('monsters', (table) => {
+  	table.increments();
+  	table.string('monster_name').notNullable();
+  	table.string('evil_power').notNullable();
+  	table.integer('number_of_arms').notNullable();
+  	table.string('monster_weapon').notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('monsters')
 };
